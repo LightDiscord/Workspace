ce32240faeb1ae9abd2835b72433bd167e15634a
LightDiscord <arnaud@lightdiscord.me>
2018-06-11 13:52:00 +0200

    Clap integration Use structopt & clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "workspace")]
+struct Workspace {
+    #[structopt(subcommand)]
+    command: Command
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// Mount a remote workspace
+    #[structopt(name = "mount")]
+    Mount
+}
+
 fn main() {
+    let matches = Workspace::from_args();
+
+    println!("{:?}", matches);
     println!("Hello, world!");
 }
